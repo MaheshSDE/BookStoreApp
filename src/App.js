8cd7356031de775009f1441ff4de7a09f2e75e29
mahesh <pmahesh493605@gmail.com>
2024-11-24 11:04:56 +0530

    Add routes to App component

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Component} from "react";
+
+import {Switch,Route,Redirect} from "react-router-dom";
+
+
+import Home from './components/Home'
+import BookList from "./components/BookList";
+import BookDetails from "./components/BookDetails";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import NotFound from "./components/NotFound";
+
+class App extends Component{
+  render(){
+    return(
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/books" component={BookList} />
+        <Route exact path="/books/:id" component={BookDetails} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/not-found" component={NotFound} />
+        <Redirect to="/not-found" />
+    </Switch>
+    )
+  }
+}
+export default App
