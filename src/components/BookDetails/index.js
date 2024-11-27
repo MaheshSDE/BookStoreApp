06e36fe759a5032a5a11c8404d4be5d2a58bab7e
mahesh <pmahesh493605@gmail.com>
2024-11-27 13:02:06 +0530

    Intialize CartContext and add cart

--- src/components/BookDetails/index.js
+++ src/components/BookDetails/index.js
@@ -5,7 +5,7 @@ import Loader from "../Loader";
 import ErrorMessage from "../ErrorMessage";
 
 import "./index.css";
-//import CartContext from "../../context/CartContext";
+import CartContext from "../../context/CartContext";
 
 const apiStatusConstants = {
   initial: "INITIAL",
@@ -62,94 +62,105 @@ class BookDetails extends Component {
 
 
     return (
-      <div className="book-details-container">
-        <div className="book-details-content-container">
-          <div className="book-basic-details-container">
-            <div className="book-details-image-container">
-              <img
-                src={image}
-                alt={title}
-                className="book-details-image"
-              />
-            </div>
-            <div className="book-other-basic-details">
-              <h1 className="book-details-heading book-details-title">
-                {title}
-              </h1>
-              <p className="book-details-subtitle">{subtitle}</p>
-              <p className="book-details-author">{authors}</p>
-              <p className="book-details-release-year">
-                Publication Year: {year}
-              </p>
-              <hr className="horizontal-rule display" />
-              <p className="book-details-price">{price}</p>
-              <div className="buttons-container">
-              <button
-                  className="book-details-button"
-                  //onClick={onClickAddToCart}
-                  //disabled={isAddedToCart}
-                >
-                  {/* {isAddedToCart ? "Added to Cart" : "Add to Cart"} */}Add to Cart
-                </button>
+      <CartContext.Consumer>
+        {(value) => {
+          const { cartList, addToCart } = value;
+          let isAddedToCart = cartList.find(eachCartItem => eachCartItem.isbn13 === this.state.bookDetailsData.isbn13)
+          const onClickAddToCart = () => {
+            addToCart({...this.state.bookDetailsData, quantity: 1})
+          }
+          return (
+            <div className="book-details-container">
+              <div className="book-details-content-container">
+                <div className="book-basic-details-container">
+                  <div className="book-details-image-container">
+                    <img
+                      src={image}
+                      alt={title}
+                      className="book-details-image"
+                    />
+                  </div>
+                  <div className="book-other-basic-details">
+                    <h1 className="book-details-heading book-details-title">
+                      {title}
+                    </h1>
+                    <p className="book-details-subtitle">{subtitle}</p>
+                    <p className="book-details-author">{authors}</p>
+                    <p className="book-details-release-year">
+                      Publication Year: {year}
+                    </p>
+                    <hr className="horizontal-rule display" />
+                    <p className="book-details-price">{price}</p>
+                    <div className="buttons-container">
+                    <button
+                        className="book-details-button"
+                        onClick={onClickAddToCart}
+                        disabled={isAddedToCart}
+                      >
+                        {isAddedToCart ? "Added to Cart" : "Add to Cart"}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <hr className="horizontal-rule" />
+                <div>
+                  <h1 className="book-details-heading">Product Description</h1>
+                  <p>{desc}</p>
+                </div>
+                <hr className="horizontal-rule" />
+                <div>
+                  <h1 className="book-details-heading">Product Details</h1>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <th>Title</th>
+                        <td>{title}</td>
+                      </tr>
+                      <tr>
+                        <th>Subtitle</th>
+                        <td>{subtitle}</td>
+                      </tr>
+                      <tr>
+                        <th>Authors</th>
+                        <td>{authors}</td>
+                      </tr>
+                      <tr>
+                        <th>Publisher</th>
+                        <td>{publisher}</td>
+                      </tr>
+                      <tr>
+                        <th>ISBN10</th>
+                        <td>{isbn10}</td>
+                      </tr>
+                      <tr>
+                        <th>ISBN13</th>
+                        <td>{isbn13}</td>
+                      </tr>
+                      <tr>
+                        <th>Pages</th>
+                        <td>{pages}</td>
+                      </tr>
+                      <tr>
+                        <th>Year</th>
+                        <td>{year}</td>
+                      </tr>
+                      <tr>
+                        <th>Rating</th>
+                        <td>{rating}</td>
+                      </tr>
+                      <tr>
+                        <th>Price</th>
+                        <td>{price}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <hr className="horizontal-rule" />
               </div>
             </div>
-          </div>
-          <hr className="horizontal-rule" />
-          <div>
-            <h1 className="book-details-heading">Product Description</h1>
-            <p>{desc}</p>
-          </div>
-          <hr className="horizontal-rule" />
-          <div>
-            <h1 className="book-details-heading">Product Details</h1>
-            <table>
-              <tbody>
-                <tr>
-                  <th>Title</th>
-                  <td>{title}</td>
-                </tr>
-                <tr>
-                  <th>Subtitle</th>
-                  <td>{subtitle}</td>
-                </tr>
-                <tr>
-                  <th>Authors</th>
-                  <td>{authors}</td>
-                </tr>
-                <tr>
-                  <th>Publisher</th>
-                  <td>{publisher}</td>
-                </tr>
-                <tr>
-                  <th>ISBN10</th>
-                  <td>{isbn10}</td>
-                </tr>
-                <tr>
-                  <th>ISBN13</th>
-                  <td>{isbn13}</td>
-                </tr>
-                <tr>
-                  <th>Pages</th>
-                  <td>{pages}</td>
-                </tr>
-                <tr>
-                  <th>Year</th>
-                  <td>{year}</td>
-                </tr>
-                <tr>
-                  <th>Rating</th>
-                  <td>{rating}</td>
-                </tr>
-                <tr>
-                  <th>Price</th>
-                  <td>{price}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <hr className="horizontal-rule" />
-        </div>
-      </div>
+          );
+        }}
+      </CartContext.Consumer>
     );
   }
 
